d340dce42a2fafdb0fe59b693131f7d42fb6d2ba
victornemeth <victor18nemeth@hotmail.com>
2025-04-01 12:39:56 +0000

    Added an undo button

--- annotations_app/urls.py
+++ annotations_app/urls.py
@@ -8,5 +8,6 @@ app_name = 'annotations_app' # Namespace for URLs
 urlpatterns = [
     path('', views.annotate_protein_view, name='annotate_protein'),
     path('annotate/', views.submit_annotation, name='annotate'),  # <-- this is the one you need
+    path('undo/', views.undo_annotation, name='undo'),
     # Add URL for submitting annotations later
 ]
