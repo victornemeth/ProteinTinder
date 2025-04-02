fe0bcaa6540af4bcde2a1aa7c3219335a72b7e3a
victornemeth <victor18nemeth@hotmail.com>
2025-04-02 08:10:24 +0000

    Added download data and checkmark

--- annotations_app/.ipynb_checkpoints/urls-checkpoint.py
+++ annotations_app/.ipynb_checkpoints/urls-checkpoint.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.view_folders, name='view_folders'),
     path('upload/', views.upload_zip_view, name='upload_zip'),
     path('folder/<int:folder_id>/annotate/', views.annotate_protein_view, name='annotate_protein'),
+    path('annotate/folder/<int:folder_id>/download/', views.download_annotations_csv, name='download_annotations_csv'),
     path('annotate/', views.submit_annotation, name='annotate'),
     path('undo/', views.undo_annotation, name='undo'),
 ]
