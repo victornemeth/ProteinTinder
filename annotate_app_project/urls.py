9524c44d0d6d0e1fb3583eaf39cc1fcaf7b73ffa
victornemeth <victor18nemeth@hotmail.com>
2025-04-04 08:08:23 +0000

    Added the option to leave and continue and reannotate afterwards

--- annotate_app_project/urls.py
+++ annotate_app_project/urls.py
@@ -9,12 +9,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', annotation_views.redirect_to_annotate, name='home_redirect'),  # 👈 This is new
+    # REMOVE or COMMENT OUT this redirect line:
+    # path('', annotation_views.redirect_to_annotate, name='home_redirect'),
+
+    # CHANGE the prefix for your app's URLs from 'annotate/' to '':
+    path('', include('annotations_app.urls', namespace='annotations_app')), # <<< CHANGE IS HERE
+
     path('admin/', admin.site.urls),
-    path('annotate/', include('annotations_app.urls', namespace='annotations_app')),
+    # REMOVE the old path that included 'annotate/' prefix:
+    # path('annotate/', include('annotations_app.urls', namespace='annotations_app')), # <<< REMOVE THIS LINE
+
+    # Keep accounts URLs
     path('accounts/signup/', annotation_views.signup_view, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+# Keep media URL config for development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
